c78a26a6e5043ac8674bf2c138cf91b812effcc9
Andrew Hamilton <ahamilton@manh.com>
2022-02-17 19:11:04 -0500

    Update Hello World Manual Examples

--- src/main/java/example/lwjgl/application/HelloWorldManualNoUtil.java
+++ src/main/java/example/lwjgl/application/HelloWorldManualNoUtil.java
@@ -2,22 +2,31 @@ package example.lwjgl.application;
 
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GL11;
 import org.lwjgl.system.MemoryUtil;
 
 import io.jadefx.JadeFX;
 import io.jadefx.scene.control.Label;
 import io.jadefx.stage.Stage;
-import io.jadefx.util.JadeFXUtil;
 
-public class HelloWorldManual {
+public class HelloWorldManualNoUtil {
 	public static void main(String[] args) {
 		
 		// Start GLFW
 		if (!GLFW.glfwInit())
 			throw new IllegalStateException("Unable to initialize GLFW");
 		
+		// Window Hints
+		GLFW.glfwDefaultWindowHints();
+		GLFW.glfwWindowHint(GLFW.GLFW_VISIBLE, GL11.GL_FALSE);
+		GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_FORWARD_COMPAT, GL11.GL_TRUE);
+		GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MAJOR, 3);
+		GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MINOR, 2);
+		GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_PROFILE, GLFW.GLFW_OPENGL_CORE_PROFILE);
+		GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_FORWARD_COMPAT, GL11.GL_TRUE);
+		long win = GLFW.glfwCreateWindow(640, 480, "Hello World Manual", MemoryUtil.NULL, MemoryUtil.NULL);
+		
 		// Create Window
-		long win = JadeFXUtil.createWindowGLFW(640, 480, "Window");
 		if ( win == MemoryUtil.NULL )
 			throw new RuntimeException("Failed to create the GLFW window");
 
